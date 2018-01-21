45c827d9538e40882ea3c1ad111d1ca7211075da
Chris <cgriggs@cs.unc.edu>
2018-01-21 09:19:48 -0500

    does it need to be in the orig prompt?

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -50,3 +50,9 @@ function hex
   set hex_value (random choice $hex_values)
   echo $hex_value
 end
+
+function fish_right_prompt
+  set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+  date "+%H:%M:%S"
+  set_color normal
+end
